aea0285df1f6365af0012741abf43f5d551eaebc
Dustin Bergman <dustin.bergman@gmail.com>
2019-06-30 14:42:58 -0700

    Initial project setup

---
+++ KEXPPower/KEXPPower.h
@@ -0,0 +1,19 @@
+//
+//  KEXPPower.h
+//  KEXPPower
+//
+//  Created by Dustin Bergman on 6/30/19.
+//  Copyright © 2019 KEXP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KEXPPower.
+FOUNDATION_EXPORT double KEXPPowerVersionNumber;
+
+//! Project version string for KEXPPower.
+FOUNDATION_EXPORT const unsigned char KEXPPowerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KEXPPower/PublicHeader.h>
+
+
---
+++ KEXPPowerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KEXPPowerExample
+//
+//  Created by Dustin Bergman on 6/30/19.
+//  Copyright © 2019 KEXP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
+
