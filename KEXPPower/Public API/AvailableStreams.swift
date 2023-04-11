fa0d3ea3413678276bbbd1b49f95abc1e4065911
Jay Kardong <jay@kexp.com>
2023-04-11 15:38:51 -0700

    Rolled back URL change for testing

--- KEXPPower/Public API/AvailableStreams.swift
+++ KEXPPower/Public API/AvailableStreams.swift
@@ -8,16 +8,15 @@
 
 import Foundation
 
-
 class AvailableStreams {
     let livePlayback: [KEXPPower.StreamingBitRate: URL]
 
     init(with listenerId: UUID) {
         var livePlaybackDict = [KEXPPower.StreamingBitRate: URL]()
         
-        livePlaybackDict[KEXPPower.StreamingBitRate.sixtyFour] = URL(string: "https://kexp.streamguys1.com/kexp64.aac?listenerId=\(listenerId.uuidString)")!
-        livePlaybackDict[KEXPPower.StreamingBitRate.oneSixty] = URL(string: "https://kexp.streamguys1.com/kexp160.aac?listenerId=\(listenerId.uuidString)")!
-        
+        livePlaybackDict[KEXPPower.StreamingBitRate.sixtyFour] = URL(string: "https://kexp.streamguys1.com/kexp64-hls/playlist.m3u8?listenerId=\(listenerId.uuidString)")!
+        livePlaybackDict[KEXPPower.StreamingBitRate.oneSixty] = URL(string: "https://kexp.streamguys1.com/kexp160-hls/playlist.m3u8?listenerId=\(listenerId.uuidString)")!
+
         livePlayback = livePlaybackDict
     }
 }
